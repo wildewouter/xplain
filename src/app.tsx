eb372a953e898c1badaff048ce6af0340842283c
Wouter de Wild <wouter@codewild.nl>
2026-09-18 22:47:42 +0200

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import {Box, Text} from 'ink';
+
+export default function App() {
+	return (
+		<Box borderStyle="round" paddingX={1}>
+			<Text>Hello, World!</Text>
+		</Box>
+	);
+}
---
+++ src/cli.tsx
@@ -0,0 +1,4 @@
+import {render} from 'ink';
+import App from './app.js';
+
+render(<App />);
